3151376724d41bb285b52aa273cc93c61f9aba7d
Oleg Sych <oleg_sych@hotmail.com>
2014-08-28 17:42:45 -0700

    Introduced Site class.

---
+++ src/RazorPress.Tests/SiteTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace RazorPress
+{
+    public class SiteTest
+    {
+        [Fact]
+        public void ClassIsPublicForCustomersToUseInTheirTemplates()
+        {
+            Assert.True(typeof(Site).IsPublic);
+        }
+
+        [Fact]
+        public void PagesIsNotNullByDefaultToPreventUsageErrors()
+        {
+            var site = new Site();
+            IList<Page> pages = site.Pages;
+            Assert.NotNull(pages);
+        }
+
+        [Fact]
+        public void PagesIsReadOnlyBecauseListItReturnsCanBeModified()
+        {
+            Assert.Null(typeof(Site).GetProperty("Pages").SetMethod);
+        }
+
+        [Fact]
+        public void TagsIsNotNullByDefaultToPreventUsageErrors()
+        {
+            var site = new Site();
+            IList<Tag> tags = site.Tags;
+            Assert.NotNull(tags);
+        }
+
+        [Fact]
+        public void TagsIsReadOnlyBecauseListItReturnsCanBeModified()
+        {
+            Assert.Null(typeof(Site).GetProperty("Tags").SetMethod);
+        }
+    }
+}
---
+++ src/RazorPress/Site.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace RazorPress
+{
+    /// <summary>
+    /// Represents a web site being generated.
+    /// </summary>
+    public class Site
+    {
+        private readonly IList<Page> pages = new List<Page>();
+        private readonly IList<Tag> tags = new List<Tag>();
+
+        /// <summary>
+        /// Gets a list of <see cref="Page"/>s.
+        /// </summary>
+        public IList<Page> Pages 
+        {
+            get { return this.pages; }
+        }
+
+        /// <summary>
+        /// Gets a list of <see cref="Tag"/>s.
+        /// </summary>
+        public IList<Tag> Tags 
+        {
+            get { return this.tags; }
+        }
+    }
+}
