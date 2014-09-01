66757d9812513a62e9ed6cfb8ca382be7b992aa0
Oleg Sych <oleg_sych@hotmail.com>
2014-09-01 14:18:32 -0700

    - Created CommandAttribute that will be used to declaratively define dependencies betwee RazorPress commands.

---
+++ src/RazorPress.Build/CommandAttribute.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.Composition;
+
+namespace RazorPress.Build
+{
+    /// <summary>
+    /// Custom export attribute for dynamic discovery of <see cref="Command"/> types through MEF.
+    /// </summary>
+    [MetadataAttribute, AttributeUsage(AttributeTargets.Class)]
+    public sealed class CommandAttribute : ExportAttribute, ICommandMetadata
+    {
+        private Type[] dependsOn = new Type[0];
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandAttribute"/> class.
+        /// </summary>
+        public CommandAttribute() : base(typeof(Command))
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets an array of <see cref="Command"/> types that should be executed before the command to which this attribute is applied.
+        /// </summary>
+        public Type[] DependsOn 
+        {
+            get 
+            { 
+                return this.dependsOn; 
+            }
+
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("value");
+                }
+
+                foreach (Type item in value)
+                {
+                    if (!typeof(Command).IsAssignableFrom(item))
+                    {
+                        throw new ArgumentException("Expecting an array of types that inherit from Command");
+                    }
+                }
+
+                this.dependsOn = value;
+            }
+        }
+    }
+}
---
+++ src/RazorPress.Build/ICommandMetadata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RazorPress.Build
+{
+    /// <summary>
+    /// Represents metadata that determines how a <see cref="Command"/> is executed.
+    /// </summary>
+    public interface ICommandMetadata
+    {
+        /// <summary>
+        /// Gets an array of <see cref="Command"/> types that should be executed before the command described by this metadata.
+        /// </summary>
+        Type[] DependsOn { get; }
+    }
+}
