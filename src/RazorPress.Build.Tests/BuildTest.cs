6fa724137eac40536e538dac2154a679bed50e6f
Oleg Sych <oleg_sych@hotmail.com>
2014-09-02 07:46:15 -0700

    - Defined Build command.

---
+++ src/RazorPress.Build.Tests/BuildTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace RazorPress.Build
+{
+    public class BuildTest
+    {
+        [Fact]
+        public void ClassIsPublicForDocumentationAndExtensibility()
+        {
+            Assert.True(typeof(Build).IsPublic);
+        }
+
+        [Fact]
+        public void ClassInheritsFromCommandForPolymorphism()
+        {
+            Assert.True(typeof(Command).IsAssignableFrom(typeof(Build)));
+        }
+
+        [Fact]
+        public void ClassDefinesCommandsItDependsOnThroughMefMetadata()
+        {
+            ICommandMetadata metadata = typeof(Build).GetCustomAttributes(false).OfType<CommandAttribute>().Single();
+            Assert.Equal(new[] { typeof(Discover), typeof(Prepare), typeof(Transform), typeof(Deploy) }, metadata.DependsOn);
+        }
+    }
+}
---
+++ src/RazorPress.Build/Build.cs
@@ -0,0 +1,10 @@
+﻿namespace RazorPress.Build
+{
+    /// <summary>
+    /// Defines RazorPress build process.
+    /// </summary>
+    [Command(DependsOn = new[] { typeof(Discover), typeof(Prepare), typeof(Transform), typeof(Deploy) })]
+    public class Build : Command
+    {
+    }
+}
