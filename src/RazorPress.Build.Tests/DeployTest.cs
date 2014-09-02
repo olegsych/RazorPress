11724012a1023c5daa3a658d304fa5b64945acea
Oleg Sych <oleg_sych@hotmail.com>
2014-09-02 07:38:52 -0700

    - Defined Deploy command

---
+++ src/RazorPress.Build.Tests/DeployTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace RazorPress.Build
+{
+    public class DeployTest
+    {
+        [Fact]
+        public void ClassIsPublicForDocumentationAndExtensibility()
+        {
+            Assert.True(typeof(Deploy).IsPublic);
+        }
+
+        [Fact]
+        public void ClassInheritsFromCommandBecauseItIsACommand()
+        {
+            Assert.True(typeof(Command).IsAssignableFrom(typeof(Deploy)));
+        }
+
+        [Fact]
+        public void ClassDefinesCommandsItDependsOnUsingMefMetadata()
+        {
+            ICommandMetadata metadata = typeof(Deploy).GetCustomAttributes(false).OfType<CommandAttribute>().Single();
+            Assert.Equal(new[] { typeof(SavePagesToDirectory) }, metadata.DependsOn);
+        }
+    }
+}
---
+++ src/RazorPress.Build/Deploy.cs
@@ -0,0 +1,10 @@
+﻿namespace RazorPress.Build
+{
+    /// <summary>
+    /// Defines deployment stage of the RazorPress build process.
+    /// </summary>
+    [Command(DependsOn = new[] { typeof(SavePagesToDirectory) })]
+    public class Deploy : Command
+    {
+    }
+}
