167a922140a719f9c5186d3883007a07d00fa067
Oleg Sych <oleg_sych@hotmail.com>
2014-09-16 07:34:09 -0700

    - Initial, incomplete implementation of ReadSiteConfiguration

---
+++ src/RazorPress.Build.Tests/ReadSiteConfigurationTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Composition;
+using System.Composition.Hosting;
+using MefBuild;
+using Xunit;
+
+namespace RazorPress.Build
+{
+    public class ReadSiteConfigurationTest
+    {
+        [Fact]
+        public void ClassIsPublicForDocumentationAndExtensibility()
+        {
+            Assert.True(typeof(ReadSiteConfiguration).IsPublic);
+        }
+
+        [Fact]
+        public void ClassInheritsFromCommandToParticipateInRazorPressBuildProcess()
+        {
+            Assert.True(typeof(Command).IsAssignableFrom(typeof(ReadSiteConfiguration)));
+        }
+
+        [Fact]
+        public void ClassIsExportedAsSharedSoThatConfigurationFileIsReadOnlyOnce()
+        {
+            CompositionContext context = new ContainerConfiguration()
+                .WithParts(typeof(ReadSiteConfiguration), typeof(Configuration))
+                .CreateContainer();
+
+            var one = context.GetExport<ReadSiteConfiguration>();
+            var two = context.GetExport<ReadSiteConfiguration>();
+            
+            Assert.Same(one, two);
+        }
+
+        [Fact]
+        public void ConfigurationIsImportedDuringComposition()
+        {
+            CompositionContext context = new ContainerConfiguration()
+                .WithParts(typeof(ReadSiteConfiguration), typeof(Configuration))
+                .CreateContainer();
+
+            var command = context.GetExport<ReadSiteConfiguration>();
+            var configuration = context.GetExport<Configuration>();
+
+            Assert.Same(configuration, command.Configuration);
+        }
+
+        // TODO: Test Execute "reads" Site.csconfig in current directory
+        // TODO: Test Execute does nothing if current directory does not contain Site.csconfig
+    }
+}
---
+++ src/RazorPress.Build/ReadSiteConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System.Composition;
+using MefBuild;
+
+namespace RazorPress.Build
+{
+    [Shared, Export]
+    public class ReadSiteConfiguration : Command
+    {
+        [Import]
+        public Configuration Configuration { get; set; }
+    }
+}
